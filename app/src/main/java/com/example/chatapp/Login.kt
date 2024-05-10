3f835eca38a2223af80dcdab589e3cd0385f7ef2
IT22251428 <it22251428@my.sliit.lk>
2024-05-10 16:08:07 +0530

    Authentication

--- app/src/main/java/com/example/chatapp/Login.kt
+++ app/src/main/java/com/example/chatapp/Login.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -12,20 +14,47 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword:EditText
     private lateinit var btnLogin:Button
     private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth //initialize firebase authentication
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        supportActionBar?.hide()
+
+
+        mAuth = FirebaseAuth.getInstance()
+
         edtEmail = findViewById(R.id.email_editText)
         edtPassword = findViewById(R.id.password_editText)
         btnLogin = findViewById(R.id.button1)
-        btnSignUp= findViewById(R.id.button2)
+        btnSignUp = findViewById(R.id.button2)
 
-        btnSignUp.setOnClickListener{
-            val intent = Intent(this,SignUp::class.java)
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password);
+        }
+    }
+    private fun login(email:String,password:String ){
+      //logic for login user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login,MainActivity2::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login,"User doesn't exist",Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
-}
+
+
+    }
--- app/src/main/java/com/example/chatapp/SignUp.kt
+++ app/src/main/java/com/example/chatapp/SignUp.kt
@@ -1,11 +1,52 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth //initialize firebase authentication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        supportActionBar?.hide()
+        mAuth = FirebaseAuth.getInstance()
+        edtEmail = findViewById(R.id.email_editText)
+        edtPassword = findViewById(R.id.password_editText)
+        edtName = findViewById(R.id.Username_editText)
+        btnSignUp = findViewById(R.id.button2)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
+        }
+    }
+    private fun signUp(email:String,password:String){
+        //logic for creating user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                   //code for jumping to home
+                    val intent = Intent(this@SignUp,MainActivity2::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@SignUp,"some error occcured", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
