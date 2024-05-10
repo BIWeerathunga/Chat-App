5b7aff295760d5ff0456e5f2eb3edcac2c00bad4
IT22251428 <it22251428@my.sliit.lk>
2024-05-10 10:07:15 +0530

    First Commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.chatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val logo = findViewById<ImageView>(R.id.imageView)
+        logo.setOnClickListener{
+            val Intent = Intent(this,Login::class.java)
+            startActivity(Intent)}
+
+
+
+    }
+}
